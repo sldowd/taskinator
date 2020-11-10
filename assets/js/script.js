20edb232d0ae1ef97a50f3d95707b99bf66f2ef4
Sarah Dowd <sarahlynnedowd@gmail.com>
2020-11-09 20:19:04 -0500

    tasks reload from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -256,7 +256,58 @@ var dragLeaveHandler = function(event) {
 var saveTasks = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+var loadTasks = function() {
+    //get task items from local storage
+    tasks = localStorage.getItem("tasks");
+    console.log(tasks);
+    if (!tasks) {
+        tasks = [];
+        return false;
+    }
+    tasks = JSON.parse(tasks);
 
+    for (var i = 0; i < tasks.length; i++) {
+        tasks[i].id = taskIdCounter;
+        console.log(tasks[i]);
+        var listItemEl = document.createElement('li');
+        listItemEl.className = 'task-item';
+        listItemEl.setAttribute("data-task-id", tasks[i].id);
+        listItemEl.setAttribute("draggable", "true");
+
+        console.log(listItemEl);
+
+        var taskInfoEl = document.createElement("div");
+        //give it a class name
+        taskInfoEl.className = "task-info";
+        //add HTML content to div
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" 
+            + tasks[i].name + "</h3><span class='task-type'>" 
+            + tasks[i].type + "</span>";
+        listItemEl.appendChild(taskInfoEl);
+        var taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+        console.log(listItemEl);
+        if (tasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "complete") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+        taskIdCounter++;
+        console.log(listItemEl);
+    }
+
+    //convert tasks from string format back into an array of objects
+
+    //iterate through a tasks array and create task elements on the page from it
+}
+loadTasks();
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
